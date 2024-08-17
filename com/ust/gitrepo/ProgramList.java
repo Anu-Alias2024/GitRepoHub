679f31cc187d9fde8c46900deb5e172e0e3c6cf7
Anu-Alias2024 <alias.aanu@gmail.com>
2024-08-17 18:21:17 +0530

    Error changed

--- com/ust/gitrepo/ProgramList.java
+++ com/ust/gitrepo/ProgramList.java
@@ -9,7 +9,6 @@ public class ProgramList {
 
 	public static void main(String[] args) {
 		try(Scanner sc = new Scanner(System.in)) {
-<<<<<<< HEAD
 			//Refactor size of list
 			int n = getSizeofthelist(sc);
 			
@@ -24,13 +23,6 @@ public class ProgramList {
 			sumOfNaturalNumbers(n, sum); 
 	        //Refactor sum of odd numbers
 	        sumofOddnumbers(numbers);
-=======
-			System.out.println("enter the limit");
-			int n =sc.nextInt();
-			
-			ArrayList<Integer> numbers = new ArrayList<>();
-			int sum =0;
-			addnumberstolist(sc, n, numbers);
 			
 			sum = sumofEvennum(numbers, sum);
 			
@@ -47,7 +39,6 @@ public class ProgramList {
 				}
 			}
 			System.out.println("Sum of odd numbers: "+sumodd);
->>>>>>> ba2d5c01d939ef5e2c35f9b5a111fcc3894e7436
 			
 			System.out.println ("The factorial of "+n+ " is : " + factorialStreams(n));
 			
@@ -63,7 +54,6 @@ public class ProgramList {
 		
 	
 	}
-<<<<<<< HEAD
 	private static void sumofOddnumbers(ArrayList<Integer> numbers) {
 		int sumodd=0;
 		
@@ -81,8 +71,6 @@ public class ProgramList {
 		int n =sc.nextInt();
 		return n;
 	}
-=======
->>>>>>> ba2d5c01d939ef5e2c35f9b5a111fcc3894e7436
 	private static void sumOfNaturalNumbers(int n, int sum) {
 		for (int i = 1; i <= n; i++) { 
 		    sum += i; 
